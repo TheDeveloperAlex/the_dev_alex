8449275bb9e69893b23697f445bd8b14303773ec
TheDeveloperAlex <83388713+TheDeveloperAlex@users.noreply.github.com>
2022-07-03 01:05:15 +0200

    add main part in index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,54 @@
-import NavBar from "../components/navBar";
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Index () {
+import photo from '../images/me.jpeg';
+
+export default function Index() {
+  
+  const linksData = [
+    { content: 'Projects', color: '#385df2', href: '/projects', isLinkWeb: false  },
+    { content: 'Github', color: '#EEA302', href: 'https://github.com/TheDeveloperAlex/', isLinkWeb: true  },
+    // { content: 'Github', color: '#EEA302', href: 'https://github.com/TheDeveloperAlex/', isLinkWeb: true  },
+  ];
+
+  const linkItems = linksData.map(({ content, color, href, isLinkWeb }) => {
+    return isLinkWeb ? (
+      <a className='default-link' href={href}>
+        <div className='circle-link' style={{ background: color }} >
+          <div className='circle-link-component' >
+            {content}
+          </div>
+        </div>
+      </a>
+    ) : (
+      <div className='circle-link' style={{ background: color }} >
+        <Link href={href}>
+          <div className='circle-link-component' >
+            {content}
+          </div>
+        </Link>
+      </div>
+    )
+  })
+
+
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div className="main-section" >
+      <div className='main-section-content-wrapper' >
+        <div className='image-wrapper'>
+          <Image src={photo} layout="fill" className='image'  />
+        </div>
+        <div className='text-wrapper' >
+          <div className='hi-text' >Hello</div>
+          <h1 className='header' >A Bit About Me</h1>
+          <div className='description' >
+            I'm 17 years old. I am a front-end web developer at Resonance. I love sports and wildlife.
+          </div>
+          <div className='buttons-wrapper' >
+            {linkItems}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/projects.js
+++ pages/projects.js
@@ -4,7 +4,7 @@ export default function Projects() {
 
 
   return (
-    <div>
+    <div className="main-section" >
       <h1>Projects</h1>
     </div>
   );
